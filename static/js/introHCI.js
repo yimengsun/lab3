0a3fab23d22b18e1dec4f510a0461fbba1af68b9
Yimeng Sun <yms001@ucsd.edu>
2019-01-24 10:50:16 -0800

    added projectclick

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -32,7 +32,8 @@ function projectClick(e) { 
        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
     } else {
        description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       
     }
 
-
+$("#myelement").toggle();
 }
